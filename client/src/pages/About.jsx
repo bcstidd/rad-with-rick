ca67b40c5412d810bfd8f93d47a91d41eee4d6a1
bcstidd <bc.stidd@gmail.com>
2025-05-30 14:20:54 -0700

    about-logo-tm addition with styling

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,6 +4,11 @@ export default function About () {
             <h2 className="about-text">About This App</h2>
             <p className="about-text">RAD With Rick is a full-stack learning platform for dental radiology review and beyond.</p>
             <p className="about-text">This application and all its contents were developed independently by Brittany Stidd and are not affiliated with or owned by any other entity.</p>
+            <div className="about-logo-container">
+            <div className="about-logo">
+                <img src="./public/icons/logo.jpg" alt="logo" />
+            </div>
+            </div>
         </div>
     )
 }
